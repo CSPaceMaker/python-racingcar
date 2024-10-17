23b4e9a7b7e5dc9b7919893497e08da738188381
YunJiUk <yjoo1123@gmail.com>
2024-10-17 13:49:25 +0900

    [#16] 시도횟수 1일때 처리 오류 해결

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -19,7 +19,7 @@ class InputHandler:
             attempts = int(input("시도할 횟수는 몇 회인가요? "))
         except ValueError:
             raise ValueError("시도 횟수는 숫자여야 합니다.")
-        if attempts < 1:
+        if attempts <= 1:
             raise ValueError("시도 횟수는 1 이상이어야 합니다.")
         return attempts
 
