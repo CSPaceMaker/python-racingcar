297c075cf2ad583e5336ee69a3d85d57bbe13f58
백승우 <swthewhite@gmail.com>
2024-10-17 10:09:02 +0900

    [#8] 경주 진행 골격 생성

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -36,6 +36,18 @@ class Car:
     def get_distance(self):
         return self.distance
 
+class Race:
+    def __init__(cars, self):
+        self.cars = cars
+    def play(self):
+        print()
+    
+def game(cars, attempts):
+    race = Race(cars)
+
+    while attempts:
+        attempts-=1
+        race.play()
 
 def main():
     """
@@ -58,7 +70,8 @@ def main():
         attempts = InputHandler.get_attempts()
     except ValueError:
         return
-
+    
+    game(cars, attempts)
 
 if __name__ == "__main__":
     # 프로그램이 직접 실행될 때만 main() 함수를 호출
